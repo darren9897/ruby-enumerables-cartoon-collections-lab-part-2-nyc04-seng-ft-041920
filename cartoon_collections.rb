5e3fd3bfc2390f67732276d30586b2bf081b4797
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-29 18:12:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -53,6 +53,11 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.each do |ele|
+    if ele == valid_calls
+      return ele
+    else
+      return nil
+    end
  
     
   end
